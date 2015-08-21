c5a298b3428e40a64d52838278f948158cb737bd
bretts61 <bretts61@gmail.com>
2015-08-21 11:54:52 -0600

    Added in tab changing/javascript/jquery

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -13,7 +13,30 @@
 #it actually doing it the first time.*/
 
 $(document).ready(function(){
-    $("#test").click(function(){
-        $(this).hide();
+    $("#statustab").click(function(){
+        $("#commands").hide();
+        $("#settings").hide();
+        $("#status").fadeIn();
+        $("#commandtab").removeClass("active");
+        $("#settingstab").removeClass("active");
+        $("#statustab").addClass("active");
     });
+    $("#commandtab").click(function(){
+        $("#status").hide();//add fancy fading or something
+        $("#settings").hide();
+        $("#commands").fadeIn();
+        $("#statustab").removeClass("active");
+        $("#settingstab").removeClass("active");
+        $("#commandtab").addClass("active");
+    });
+    $("#settingstab").click(function(){
+        $("#status").hide();
+        $("#commands").hide();
+        $("#settings").fadeIn();
+        $("#statustab").removeClass("active");
+        $("#commandtab").removeClass("active");
+        $("#settingstab").addClass("active");
+    });
+    $("#commands").hide();
+    $("#settings").hide();
 });
